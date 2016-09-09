3255d04690bb86e5c85b2b0e4f17db68b071f964
이동욱 <jojoldu@zuminternet.com>
2016-09-09 18:05:51 +0900

    프로젝트 세팅

---
+++ src/main/java/dev/controller/HomeController.java
@@ -0,0 +1,17 @@
+package dev.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jojoldu@gmail.com on 2016-08-30.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/hooks")
+    public
+}
---
+++ src/test/java/dev/DevbeginnerApplicationTests.java
@@ -0,0 +1,16 @@
+package dev;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevbeginnerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
