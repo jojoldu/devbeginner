ad8d4afa4022c8290c6ea821fe1bf318799d5ba3
이동욱 <jojoldu@gmail.com>
2016-12-23 00:29:15 +0900

    Facebook 회원 정보 가져오기 진행중

--- src/main/java/com/blogcode/api/ApiController.java
+++ src/main/java/com/blogcode/api/ApiController.java
@@ -1,7 +1,5 @@
 package com.blogcode.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +16,9 @@ public class ApiController {
 
     private UserDetailsService userDetailsService;
 
-    public void setUserDetailsService(UserDetailsService userDetailsService){
-        this.userDetailsService = userDetailsService;
-    }
 
     @GetMapping("/user")
-    public UserDetails getUser(Principal principal){
-        return userDetailsService.loadUserByUsername(principal.getName());
+    public Principal getUser(Principal principal){
+        return principal;
     }
 }
---
+++ src/main/java/com/blogcode/oauth/FacebookRepository.java
@@ -0,0 +1,13 @@
+package com.blogcode.oauth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by jojoldu@gmail.com on 2016-12-23.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+
+public interface FacebookRepository  {
+}
